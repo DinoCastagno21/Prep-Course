77edad5539e2bdd5df19b4c60668b4d416440eae
Dino Castagno <dinocastagnodinocastagno93@gmail.com>
2022-08-23 14:37:40 -0300

    finish homework repaso

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,9 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  var matriz = Object.entries(objeto);
+  
+  return matriz;
 }
 
 
@@ -18,6 +21,15 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  var obj = {};
+  for(var i = 0; i < string.length; i++) {
+    if(!obj.hasOwnProperty(string[i])) {
+      obj[string[i]] = 0;
+    }
+    obj[string[i]] += 1;
+  }
+  return obj;
+  
 }
 
 
@@ -26,6 +38,16 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  var mayus = '';
+  var minus = '';
+  for(var i = 0; i < s.length; i++){
+    if(s[i] === s[i].toUpperCase()){
+      mayus += s[i];
+    }else{
+      minus += s[i];
+    }
+  }
+  return mayus + minus;
 }
 
 
@@ -35,6 +57,19 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+  
+  var  fraseInvertida = str.split(" ").map(function(palabra){
+    // var cadapalabra ='';
+    // for(var i = palabra.length - 1; i >= 0; i--) {
+      
+    //   cadapalabra = cadapalabra + palabra[i];
+
+    // }
+    // return cadapalabra;
+    return palabra.split("").reverse().join('');
+  }).join(" ")
+
+  return fraseInvertida;
 } 
 
 
@@ -43,13 +78,30 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
-}
+  var capicua = numero.toString();
+  if (capicua.split('').join() === capicua.split('').reverse().join()){
+    return "Es capicua";
+  }else{
+    return "No es capicua";
+  }
+  }
+
+
 
 
 function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  var cadenadamodificada = '';
+  for(var i = 0; i < cadena.length; i++){
+    if(cadena[i] === "a" || cadena[i] === "b" || cadena[i] === "c"){
+      continue;
+    }else{
+      cadenadamodificada = cadenadamodificada + cadena[i];
+    }
+  }
+  return cadenadamodificada;
 }
 
 
@@ -57,7 +109,22 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
-}
+  
+  aux = '';
+  
+   for(var i = 0 ; i < arr.length -1 ; i++){
+      for(var j = 0; j < arr.length -1; j++){
+        if(arr[j].length > arr[j+1].length){
+          aux = arr[j]
+          arr[j] = arr[j+1]
+          arr[j+1] = aux
+       }
+      } 
+    }
+    return arr;
+//metodo burbuja
+  }
+
 
 
 function buscoInterseccion(arreglo1, arreglo2){
@@ -65,7 +132,18 @@ function buscoInterseccion(arreglo1, arreglo2){
   //retornar un nuevo array con la intersección de ambos elementos. (Ej: [4,2,3] unión [1,3,4] = [3,4].
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
-  //Escribe tu código aquí  
+  //Escribe tu código aquí
+  var arregloInterseccion = [];
+  for(var i = 0; i < arreglo1.length;i++){
+    for(var j = 0; j < arreglo2.length;j++){
+      if(arreglo1[i]===arreglo2[j]){
+        arregloInterseccion.push(arreglo1[i]);
+      }
+    }
+  }
+  
+
+  return arregloInterseccion;
 }
 
 
